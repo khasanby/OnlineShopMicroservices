5c0b0cf398272fcf5dff0d995892206de1c5e8b0
khasanby <69844489+khasanby@users.noreply.github.com>
2025-04-23 18:37:26 +0500

    seeding db with Marten

--- src/Services/Inventory/Inventory.API/Data/CatalogInitialData.cs
+++ src/Services/Inventory/Inventory.API/Data/CatalogInitialData.cs
@@ -1,6 +1,20 @@
-﻿namespace Inventory.API.Data;
+﻿using Marten.Schema;
 
-public sealed class CatalogInitialData
+namespace Inventory.API.Data;
+
+public sealed class CatalogInitialData : IInitialData
 {
-    
+    private readonly Product[] _initialData;
+
+    public CatalogInitialData(Product[] initialData)
+    {
+        _initialData = initialData;
+    }
+
+    public async Task Populate(IDocumentStore store, CancellationToken cancellation)
+    {
+        await using var session = store.LightweightSession();
+        session.Store(_initialData);
+        await session.SaveChangesAsync();
+    }
 }
