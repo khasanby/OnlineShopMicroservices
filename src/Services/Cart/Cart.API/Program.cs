de0d598f8f5c95c7b36bac83f26b9411622c2952
khasanby <69844489+khasanby@users.noreply.github.com>
2025-04-25 18:57:04 +0500

    dockerized and added orchestration for cart api.

--- src/Services/Cart/Cart.API/Program.cs
+++ src/Services/Cart/Cart.API/Program.cs
@@ -1,9 +1,40 @@
+using BuildingBlocks.Behaviors;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var assembly = typeof(Program).Assembly;
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+});
+
+builder.Services.AddMarten(opts =>
+{
+    opts.Connection(builder.Configuration.GetConnectionString("Database")!);
+    opts.Schema.For<ShoppingCart>().Identity(x => x.Username);
+}).UseLightweightSessions();
+
+builder.Services.AddScoped<ICartRepository, CartRepository>();
+
+builder.Services.AddHealthChecks()
+    .AddNpgSql(builder.Configuration.GetConnectionString("CartDb")!);
+
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.MapCarter();
+app.UseExceptionHandler(options => { });
+app.UseHealthChecks("/health",
+    new HealthCheckOptions
+    {
+        ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+    });
 
 app.Run();
